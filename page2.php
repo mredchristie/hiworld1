6996042436d7ec0635698624f32df4ba91bce7d1
mredchristie <45788302+mredchristie@users.noreply.github.com>
2021-11-30 16:01:04 +0000

    finished monday  30/11/2021
    finished monday  30/11/2021

--- page2.php
+++ page2.php
@@ -16,18 +16,21 @@
 
 print_r($_POST);
 
-
+// the credentials to connect to the database - the database name is very important as I found out the hardway
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "testfestival";
 ?>
 
+
+<!-- need to remeber that if I want to add somem html within my php then I need to end off ?> the php and you can embed php like  -->
 <hr>
 fname = <?= $_POST['fname'] ?>
 <br>
 lname = <?= $_POST['lname']?>
 <br>
+<!-- this is just outputting the data of the php variables lname and fname -->
 
 lname = <?php print( $_POST['lname'] ); ?>
 
