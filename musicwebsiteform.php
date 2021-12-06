38b30b8ded594b44b32c2371c95380372c8c4238
mredchristie <45788302+mredchristie@users.noreply.github.com>
2021-12-06 10:34:14 +0000

    hopefully all fixed
    06/12/2021 10:34

--- musicwebsiteform.php
+++ musicwebsiteform.php
@@ -7,7 +7,6 @@
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="stylesmusicwebsiteform.css" rel="stylesheet" />
     <link href="stylesmusicwebsiteinfopage.css" rel="stylesheet" />
