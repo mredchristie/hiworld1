afcfa248a6e853f742619406762443ddd0e2f8b9
mredchristie <45788302+mredchristie@users.noreply.github.com>
2021-12-17 13:33:34 +0000

    yes
    uploadd

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link href="stylesmusicwebsiteform.css" rel="stylesheet" />
     <link href="stylesmusicwebsiteinfopage.css" rel="stylesheet" />
@@ -40,12 +39,6 @@
                   </svg>
                 </a>
               </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#"> Basket <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                  </svg>
-                  </a>
-              </li>       
             </ul>
           </div>
         </div>
