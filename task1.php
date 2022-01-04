34b654db6b9fc9badee54146cb79fd3c964c0000
mredchristie <45788302+mredchristie@users.noreply.github.com>
2022-01-04 09:34:04 +0000

    Task1.php Partly done
    added php and some imbedded html within php

---
+++ task1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<p>This is standard HTML</p>
+
+<?php
+echo 'This was generated using PHP!';
+echo '<h1>THIS IS A TITLE with embeded html in php </h1>';
+?>
+
+
+
+    
+</body>
+</html>
